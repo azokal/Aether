907a243578d43cc6e62e5f38d801b560312c0b7a
Maxine Constantinian <maxime.constantinian@gmail.com>
2025-01-21 23:44:15 +0100

    Add documentation for SceneManager

--- core/scene_manager.lua
+++ core/scene_manager.lua
@@ -1,9 +1,13 @@
+---The SceneManager have to manipulate scene and define the current one
 ---@class SceneManager
----@field public class_name string
----@field public scenes table[Scene]
----@field public current_scene string
+---@field public class_name string The Class Name
+---@field public scenes { [string]: Scene }|nil All scenes track by the SceneManager
+---@field public current_scene string Name of the current scene
 SceneManager = { class_name = "SceneManager", scenes = nil, current_scene = "" }
 
+---SceneManager constructor
+---@param o table Model used for the copy
+---@return SceneManager o SceneManager instance
 function SceneManager:new(o)
     o = o or {}
     o.scenes = {}
@@ -12,15 +16,22 @@ function SceneManager:new(o)
     return o
 end
 
+---Add Scene to the SceneManager
+---@param name string The scene's name. It is used as a key
+---@param scene Scene Scene object
 function SceneManager:addScene(name, scene)
     self.scenes[name] = scene
 end
 
+---Change the current Scene
+---@param name string The scene's name for the new current scene
 function SceneManager:changeScene(name)
     self.current_scene = name
     self.scenes[name]:init()
 end
 
+---Get the current scene
+---@return Scene current_scene The current scene
 function SceneManager:getCurrentScene()
     return self.scenes[self.current_scene]
 end
