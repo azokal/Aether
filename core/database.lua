8976dab57e362c93a55204ae9941fdbd5d5533cc
Maxine Constantinian <maxime.constantinian@gmail.com>
2025-01-24 13:21:46 +0100

    Add documentation for Database

--- core/database.lua
+++ core/database.lua
@@ -1,10 +1,18 @@
 require("Aether.utils")
 
+---Database is Class used to preload data and storage data during the game runtime
+---@class Database
+---@field public class_name string The class name
+---@field public datas table? The data storage
 Database = { class_name = "Database", datas = nil }
 
-require("data.init")
+---Add the init function to Database
+require("Aether.data.init")
 
-function Database:new(o, w, h)
+---Database constructor
+---@param o table? Table model used for the copy
+---@return Database database The Database instance
+function Database:new(o)
     o = o or {}
     o.datas = {}
     setmetatable(o, self)
@@ -12,6 +20,10 @@ function Database:new(o, w, h)
     return o
 end
 
+---Load a new file in Database
+---@param input string Prefix for the key
+---@param path string File's path to load
+---@param data table Append new data to his data
 function Database:load(input, path, data)
     data = data or self.datas
 
@@ -22,7 +34,11 @@ function Database:load(input, path, data)
     config(data[input], self)
 end
 
+---Get data from specified datapath
+---@param datapath string Datapath with "." separator
+---@return table data The specified data
 function Database:get(datapath)
+    ---@type table
     local ret = self.datas
     local p = datapath:split(".")
     for i = 1, #p, 1 do
