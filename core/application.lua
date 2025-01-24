7f61c3af38b5adf66d9cb5a0878729d09dfd3a4c
Maxine Constantinian <maxime.constantinian@gmail.com>
2025-01-24 13:33:43 +0100

    Add documentation for Application

--- core/application.lua
+++ core/application.lua
@@ -4,15 +4,23 @@ require("Aether.core.resource")
 require("Aether.core.database")
 require("Aether.core.events")
 
+---Main Aether's Application where we can find all anothers Aether's manager
 ---@class Application
 ---@field public class_name string
----@field public scene_manager SceneManager
----@field public resource Resource
----@field public mouse Mouse
+---@field public scene_manager? SceneManager
+---@field public resource? Resource
+---@field public database? Database
+---@field public events? Events
+---@field public input? Input
 ---@field public base_width number
 ---@field public base_height number
 Application = { class_name = "Application", scene_manager = nil, resource = nil, database = nil, events = nil, input = nil, base_width = 0, base_height = 0 }
 
+---Application constructor
+---@param o table? Table model used for the copy
+---@param w number The base resolution's width
+---@param h number The base resolution's height
+---@return Application app Application instance
 function Application:new(o, w, h)
     o = o or {}
     o.scene_manager = SceneManager:new()
